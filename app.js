03f8a7972a4f63e1c10b10d2fe7beb7220537069
Ihimekpen Osemudiamen Andrew <oihimekpen@gmail.com>
2022-03-09 17:41:26 +0100

    sql connection

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require("express");
 var mysql      = require('mysql'); 
 
 const app = express();
-const url =
-// var connection = mysql.createConnection({
-//     host     : 'localhost',
-//     user     : 'me',
-//     password : 'secret',
-//     database : 'my_db'
-//   });
+const url = " mysql://b3a376e1086f60:cf91152e@us-cdbr-east-05.cleardb.net/heroku_ad7e80e651ce74d?reconnect=true";
+
+var connection = mysql.createConnection({
+    host     : 'us-cdbr-east-05.cleardb.net',
+    username : 'b3a376e1086f60',
+    password : 'cf91152e',
+    database : 'heroku_ad7e80e651ce74d'
+  });
    
-//   connection.connect();
-  mysql.connect(url);
+  connection.connect();
+//   mysql.connect(url);
    
   
