2e67f726279e15116a65fa533501bd99ed442b10
Ihimekpen Osemudiamen Andrew <oihimekpen@gmail.com>
2022-03-09 21:15:38 +0100

    sql setup

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 var mysql      = require('mysql'); 
+require("dotenv").config();//to use environmental variables
+
 
 const app = express();
 const url = " mysql://b3a376e1086f60:cf91152e@us-cdbr-east-05.cleardb.net/heroku_ad7e80e651ce74d?reconnect=true";
@@ -7,11 +9,25 @@ const url = " mysql://b3a376e1086f60:cf91152e@us-cdbr-east-05.cleardb.net/heroku
 var connection = mysql.createConnection({
     host     : 'us-cdbr-east-05.cleardb.net',
     username : 'b3a376e1086f60',
-    password : 'cf91152e',
+    password : '',
     database : 'heroku_ad7e80e651ce74d'
   });
    
-  connection.connect();
+
 //   mysql.connect(url);
    
-  
+// production
+
+// This code creates a server listening at the  port specified
+app.listen(process.env.PORT, process.env.LOCAL_ADDRESS, () => {
+  console.log(`Server is now listening at port ${process.env.PORT}`);
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
